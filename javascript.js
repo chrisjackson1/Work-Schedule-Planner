feafdbe475004c1137e9340278487c59d9589845
Chris Jackson <chrisjackson@Chriss-MBP.fios-router.home>
2022-05-25 15:17:52 -0400

    updates

---
+++ javascript.js
@@ -0,0 +1,14 @@
+
+
+var todaysDate = moment().format("MMMM Do, YYYY");
+$("#currentDay").text("Today is " + TodaysDate);
+
+function colorSchedule(){
+
+}
+
+moment().format("H");
+
+colorSchedule();
+
+
