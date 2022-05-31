07dfba494a4fb00610848c4bb9c826c79dcadf2a
Chris Jackson <chrisjackson@Chriss-MBP.fios-router.home>
2022-05-31 14:10:23 -0400

    edited demo work

--- javascript.js
+++ javascript.js
@@ -1,14 +1,33 @@
-
-
+var times = document.getElementsByClassName('time')
+var buttons = document.getElementsByClassName('buttons')
 var todaysDate = moment().format("MMMM Do, YYYY");
-$("#currentDay").text("Today is " + TodaysDate);
+$("#currentDay").text("Today is " + todaysDate);
 
-function colorSchedule(){
+
+
+//Get the current day in a nice format and 
+var currenTime= moment().format("LT");
+console.log(currenTime)
+
+function checkIfPast(){
+for(i = 0; i < times.length; i++){
+    if(currenTime > times[i].innerHTML){
+        console.log(times[i].nextElementSibling)
+        times[i].nextElementSibling.classList.add("past")
+    } 
+        
+    }
+} 
+
+function checkIfFuture(){
+    
+}
+
+function saveTask(e){
+    buttons.addEventListener("click", saveTask)
 
 }
 
-moment().format("H");
 
-colorSchedule();
 
 
